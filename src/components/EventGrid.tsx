bc04eba9a04021af76b164b7b8cd8e770000a542
Julien Habibi <julien.hi@yahoo.com>
2026-09-04 21:14:14 +0200

    fix: update grid col

--- src/components/EventGrid.tsx
+++ src/components/EventGrid.tsx
@@ -51,7 +51,7 @@ export function EventGrid({
           </p>
         ) : (
           <>
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
               {events.map((event, index) => (
                 <EventCard
                   key={event.id}
