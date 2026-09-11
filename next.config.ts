ffd7d9900e0d36c75c58ec7609e51159e0e3c0a1
Julien Habibi <julien.hi@yahoo.com>
2026-09-11 22:43:09 +0200

    fix: allow Bouillon remote images

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.openagenda.com",
       },
+      {
+        protocol: "https",
+        hostname: "www.univ-orleans.fr",
+      },
     ],
   },
 };
